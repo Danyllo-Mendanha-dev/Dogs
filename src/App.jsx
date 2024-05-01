43540d8bad348904f7712f370f9fff110a66b8be
Danyllo-Mendanha-dev <danyllomendanha10@gmail.com>
2024-05-01 15:55:30 -0300

    estrutura-inicial

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
+import Home from "./Components/Home"
+import Login from "./Components/Login/Login.jsx";
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App;
