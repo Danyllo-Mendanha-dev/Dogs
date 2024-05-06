e83e858d060a99faa5398a39b6d628b3346f74f7
Danyllo-Mendanha-dev <danyllomendanha10@gmail.com>
2024-05-06 14:43:52 -0300

    formComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import "./App.css"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./Components/Header.jsx";
-import Footer from "./Components/Footer.jsx";
-import Home from "./Components/Home"
-import Login from "./Components/Login/Login.jsx";
-const App = () => {
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
+function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login/*" element={<Login />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
   );
-};
+}
+
 export default App;
