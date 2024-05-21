363ee9a04c7d6cbe2b10ea393acb854a4e0a9bf4
Danyllo-Mendanha-dev <danyllomendanha10@gmail.com>
2024-05-21 20:30:28 -0300

    scroll-infinito

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,21 +1,24 @@
-import React from "react"
-import UserHeader from "./UserHeader"
-import { Route, Routes } from "react-router-dom"
-import Feed from "../Feed/Feed.jsx"
-import UserPhotoPost from "../User/UserPhotoPost.jsx"
-import UserStats from "./UserStats.jsx"
+import React from "react";
+import UserHeader from "./UserHeader";
+import { Route, Routes } from "react-router-dom";
+import Feed from "../Feed/Feed.jsx";
+import UserPhotoPost from "../User/UserPhotoPost.jsx";
+import UserStats from "./UserStats.jsx";
+import { UserContext } from "../../UserContext.jsx";
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />}/>
-        <Route path="postar" element={<UserPhotoPost />}/>
-        <Route path="estatísticas" element={<UserStats />}/>
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatísticas" element={<UserStats />} />
       </Routes>
     </section>
-  )
-}
+  );
+};
 
-export default User
+export default User;
