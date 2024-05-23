c85e31fb97137119c1b27ea3cd35248c404420cb
Danyllo-Mendanha-dev <danyllomendanha10@gmail.com>
2024-05-23 20:32:12 -0300

    bibliotecaExterna02

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Head from "../Helper/Head";
-import useFetch from "../../Hooks/useFetch";
-import Loading from "../Helper/Loading";
-import Error from "../Helper/Error";
-import UserStatsGraphs from "./UserStatsGraphs";
+import React from 'react';
+import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -11,7 +12,7 @@ const UserStats = () => {
   React.useEffect(() => {
     async function getData() {
       const { url, options } = STATS_GET();
-      request(url, options);
+      await request(url, options);
     }
     getData();
   }, [request]);
@@ -22,7 +23,7 @@ const UserStats = () => {
     return (
       <div>
         <Head title="Estatísticas" />
-        <UserStatsGraphs data={data} /> 
+        <UserStatsGraphs data={data} />
       </div>
     );
   else return null;
