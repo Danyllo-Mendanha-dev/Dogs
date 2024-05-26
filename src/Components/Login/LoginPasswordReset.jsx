b81401c4825b625dba53d8d82a19b95e1a5ddc34
Danyllo-Mendanha-dev <danyllomendanha10@gmail.com>
2024-05-26 13:12:39 -0300

    ajustesFinais

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -37,11 +37,16 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete a senha" />
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Nova Senha" type="password" name="password" {...password} />
+        <Input
+          label="Nova Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
         {loading ? (
           <Button disabled>Resetando...</Button>
         ) : (
@@ -49,7 +54,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
